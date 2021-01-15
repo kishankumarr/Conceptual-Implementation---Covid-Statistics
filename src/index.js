0a718c6b7d253ff748cdbf6b1519bc6b58e4ade3
kishan kumar a <kishankumar12324@gmail.com>
2021-01-15 23:20:24 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -8,9 +8,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connection } = require('./connector')
 
+const covidTallyModel=connection;
+app.get("/totalrecovered",async(req,res)=>{
+    const resDoc=await covidTallyModel.aggregate([
+        {
+            $group:{
+                _id:"total",
+                recovered:{$sum:"$recovered"},
+               count:{$sum:1}
+            },
+        },
+    ]);
+    res.send(resDoc);
+});
 
-
-
+app.get("/totalActive",async(req,res)=>{
+    const resDoc=await covidTallyModel.aggregate([
+        {
+            $group:{
+                _id:"total",
+                recovered:{$sum:"$recovered"},
+                infected:{$sum:"$infected"},
+            }
+    },
+    ]);
+    const Result=resDoc[0];
+    res.send({data:{_id:"total",active:Result.infected-Result.recoverd}});
+});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
